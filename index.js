ef5b6091390b315f4134dc9c27bf22c5dfad6ecd
fantom2006 <alibaeveldiar01@gmail.com>
2023-07-03 16:33:09 +0600

    dz-1

---
+++ index.js
@@ -0,0 +1,75 @@
+1
+
+// let name = prompt('Enter your first name')
+// let surname = prompt('Enter your surname')
+
+// alert(name + ' ' + surname + " " + "Здравсвтуйте")
+
+// 2
+
+// let firstnum = prompt('enter first number')
+// let secondnum = prompt('enter second number')
+ 
+// if(firstnum > secondnum){
+//     alert(firstnum + " " + "больше чем" + " " + secondnum)
+// }
+// else if(firstnum < secondnum){
+//     alert(firstnum + " " + "меньше чем" + " " + secondnum)
+// }
+// else{
+//     alert("они равны")
+// }
+
+
+// 3
+
+// let light = prompt('Enter the color')
+
+// switch (light) {
+//     case ('красный'):
+//         alert('стоять');
+//         break;
+//     case ('зеленный'):
+//         alert('идти');
+//         break;
+//     case('желтый'):
+//         alert('ждать');
+//         break;
+//  default:
+//         break;
+// }
+ 
+
+// допзд 
+
+// let light = prompt('Введите количество звезд')
+
+// switch (light) {
+//     case ('1'):
+//         alert('20$');
+//         break;
+//     case ('2'):
+//         alert('30$');
+//         break;   
+//     case ('3'):
+//         alert('50$');
+//         break;
+//     case ('4'):
+//         alert('70$');
+//         break;
+//     case ('5'):
+//         alert('120$');
+//         break;
+//     case ('1,2'):
+//         alert('20$');
+//         break;
+//     case ('3,4'):
+//         alert('30$');
+//         break;   
+//     case ('5'):
+//         alert('120$');
+//         break;
+// default:
+//         alert("Такого количества звезд нет");
+//         break;
+// }
